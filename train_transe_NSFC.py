ec7c8076436ee01df9fa90a5cadcf8c3865078c5
rjagge <rjagge@163.com>
2023-02-20 11:15:01 +0800

    updates

---
+++ train_transe_NSFC.py
@@ -0,0 +1,60 @@
+'''
+Date: 2022-12-11 15:24:32
+LastEditors: Jagger
+Description: 
+LastEditTime: 2023-02-10 14:22:28
+FilePath: /nsfc-kg/OpenKE/train_transe_NSFC.py
+'''
+import openke
+from openke.config import Trainer, Tester
+from openke.module.model import TransE
+from openke.module.loss import MarginLoss
+from openke.module.strategy import NegativeSampling
+from openke.data import TrainDataLoader, TestDataLoader
+
+# 每次train新的checkpoint的时候需要把对应的folder中的train修改一下
+# cp train2id_3.txt train2id.txt
+checkpoint = './checkpoint/nsfc/transe_100w.ckpt'
+new_checkpoint = './checkpoint/nsfc/transe_150w.ckpt'
+
+# dataloader for training
+train_dataloader = TrainDataLoader(
+	in_path = "/home/user4/data/research/nsfc-kg/OpenKE/benchmarks/NSFC/", 
+	nbatches = 100,
+	threads = 8, 
+	sampling_mode = "normal", 
+	bern_flag = 1, 
+	filter_flag = 1, 
+	neg_ent = 25,
+	neg_rel = 0)
+
+# dataloader for test
+# test_dataloader = TestDataLoader("/home/user4/data/research/nsfc-kg/OpenKE/benchmarks/NSFC/", "link")
+
+# define the model
+transe = TransE(
+	ent_tot = train_dataloader.get_ent_tot(),
+	rel_tot = train_dataloader.get_rel_tot(),
+	dim = 200, 
+	p_norm = 1, 
+	norm_flag = True)
+transe.load_checkpoint(checkpoint)
+
+
+# define the loss function
+model = NegativeSampling(
+
+	model = transe, 
+	loss = MarginLoss(margin = 5.0),
+	batch_size = train_dataloader.get_batch_size()
+)
+
+# train the model
+trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = True)
+trainer.run()
+transe.save_checkpoint(new_checkpoint)
+
+# test the model
+# transe.load_checkpoint('./checkpoint/nsfc/transe.ckpt')
+# tester = Tester(model = transe, data_loader = test_dataloader, use_gpu = True)
+# tester.run_link_prediction(type_constrain = False)
